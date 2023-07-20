f97443b895971a3777cfe1c382df7825775d300c
Fahad-Iqbal <fahadiq@hotmail.com>
2023-07-20 19:41:20 -0400

    reset the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import { useState } from 'react';
 import './App.css';
-import Document from './Document';
-import Header from './Header';
-import Sidebar from './Sidebar';
-import Sidebar2 from './Sidebar2';
 
 export default function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   return (
     <main>
-      <Sidebar2
-        isSidebarOpen={isSidebarOpen}
-        setIsSidebarOpen={setIsSidebarOpen}
-      />
-
-      <div className="main-container">
-        <Header />
-        <Document />
-      </div>
+      <h1>Flashnote</h1>
     </main>
   );
 }
